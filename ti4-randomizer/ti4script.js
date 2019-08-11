99447e9edcf6c92a902abc4d80e23547829d03e7
jacobwilsonhamilton <jacobwilsonhamilton@gmail.com>
2019-08-11 10:54:44 -0500

    randomizer updates

--- ti4-randomizer/ti4script.js
+++ ti4-randomizer/ti4script.js
@@ -1,58 +1,46 @@
-    
-const bottomContainer = document.querySelector('.bottom-container');
-const playerCounter = document.querySelector('#player-counter');
-const raceListCard = document.querySelector('.setting-inputs');
-const resultListContainer = document.querySelector('.results-container');
+    const bottomContainer = document.querySelector('.bottom-container');
+    const playerCounter = document.querySelector('#player-counter');
+    const raceListCard = document.querySelector('.setting-inputs');
+    const resultListContainer = document.querySelector('.results-container');
+    const nameInputContainer = document.querySelector('.player-details-container');
+    const raceDisplay = document.getElementById('result-list');
 
-const raceDisplay = document.getElementById('result-list');
-
-let counter = playerCounter.addEventListener('change',() => {
-    let playerCount = parseInt(playerCounter.value); 
-    return addInputs(playerCount);
-});
+    let counter = playerCounter.addEventListener('change', () => {
+        let playerCount = parseInt(playerCounter.value);
+        return addInputs(playerCount);
+    });
 
 
 function getRaces(frm) {
+    //iterate over checklist of races, snag the checked ones, note that for loop is preferable to forEach here because need to reference index of each checklist item/race
     const raceChecks = frm.getElementsByClassName('raceBox');
-    console.log(raceChecks);
     let selectBoxes = [];
 
     for (let i = 0; i < raceChecks.length; i++) {
-       if (raceChecks[i].type == 'checkbox' && raceChecks[i].checked == true) {
-           selectBoxes.push(raceChecks[i].value)
-       }
-        // console.log(selectBoxes);
+        if (raceChecks[i].type == 'checkbox' && raceChecks[i].checked == true) {
+            selectBoxes.push(raceChecks[i].value);
+        }
     }
-    console.log(selectBoxes);
-    console.log(showRaces(selectBoxes));
-
-    resultListContainer.style.display="block";
-    
-}   
-
-function showRaces(lister) {
-    const raceDisplay = document.getElementById('result-list');
-    let i = 0;
-    let newList = [];
-    lister.forEach(race => {
-        newList[i] = race + '';
-        i++;
-    });
-    // raceListCard.style.display="none";
-    
-    raceDisplay.innerHTML = newList.join('</br>');
-  
- console.log(raceDisplay.innerHTML);
- return raceDisplay.innerHTML;
+   showRaces(selectBoxes);
 }
 
-const nameInputContainer = document.querySelector('.player-details-container');
-function addInputs (count) {
-//for each player, add text input html to output array, each with player ID label/Id/Name/selectors incremented accordingly.
-//Then join the array elems and write it to the inner html of the 'player-details-container' div
-    
+function showRaces(raceList) {
+// join the array of races + br tags creates in getRaces into single string, write it to appropriate html element. Then update resultContainer to display.
+    const raceDisplay = document.getElementById('result-list');
+    // raceListCard.style.display="none";
+
+    raceDisplay.innerHTML = raceList.join('</br>');
+    resultListContainer.style.display = "block";
+
+}
+
+
+
+function addInputs(count) {
+    /*for each player, 1) add text input html to output array, each with player ID label/Id/Name/selectors incremented accordingly.
+        Then 2) join the array elems and write it to the inner html of the 'player-details-container' div */
     const output = [];
-    
+
     for (let i = 1; i <= count; i++) {
         output.push(`
             <label for="playername-textbox1">Player ${i} 
@@ -60,7 +48,6 @@ function addInputs (count) {
                     placeholder="Enter a name...">
             </label><br>         
         `);
-        
     }
     nameInputContainer.innerHTML = output.join('');
 }
