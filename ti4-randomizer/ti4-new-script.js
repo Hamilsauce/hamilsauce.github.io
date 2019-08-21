89648cfdd4950fcf438eff049e83b68de35adcb6
jacobwilsonhamilton <jacobwilsonhamilton@gmail.com>
2019-08-21 06:04:12 -0500

    updates to rando

--- ti4-randomizer/ti4-new-script.js
+++ ti4-randomizer/ti4-new-script.js
@@ -5,11 +5,15 @@ const nameInputContainer = document.querySelector('.player-details-container');
 const raceDisplay = document.getElementById('result-list');
 const submitButton = document.querySelector('#submit-btn');
 const resetButton = document.querySelector('#reset-btn');
+const selectAllButton = document.querySelector('#select-btn');
+const clearButton = document.querySelector('#clear-btn');
+//const raceItems = document.querySelectorAll('.raceBox');
 
 let textBoxes;
 let playerNames = [];
 let players = [];
 let raceList = [];
+let usedBoxes = [];
 
 let nameBoxes = playerCounter.addEventListener('change', () => {
     //Changes name textboxes displayed when player counter changes, returns the textbox form elements 
@@ -19,24 +23,83 @@ let nameBoxes = playerCounter.addEventListener('change', () => {
 
 submitButton.addEventListener('click', () => {
     //on submit button, dole races and display 
+
+    let nameArr = trimNames(playerNames);
+    console.log(trimNames);
+
     raceList = getRaceList()
-    getPlayerData(playerNames, raceList);
+    addPlayer(nameArr, raceList);
     doleOut(raceList);
     displays();
 });
 
+
+selectAllButton.addEventListener('click', () => {
+    const raceItems = document.querySelectorAll('.raceBox');
+
+
+    raceItems.forEach(r => {
+        r.checked = true;
+    });
+    return;
+});
+
+
+clearButton.addEventListener('click', () => {
+    const raceItems = document.querySelectorAll('.raceBox');
+
+
+    raceItems.forEach(r => {
+        r.checked = false;
+    });
+    return;
+});
+
+
 function getPlayerNames(boxes) {
     //function is run everytime a user deselects a text input - adds text content to playername list 
-
     let box = document.querySelector(boxes);
-    let input = box.value;
-    if (playerNames.length >= 6) {
-        alert("Already Got enough players. Click Reset to start over.");
-        return;
-    } else if (input != "") {
-        playerNames.push(input);
+    //  let document.querySelectorAll('.name-input-field')]m0;
+    let [bName, bVal, pos] = [box.name, box.value, parseInt(box.name.split('').pop()) - 1];
+
+    if (bVal == '' || bVal === undefined) /*&& playerNames[pos].length > 1) */ {
+        console.log(`pName elem  = ${playerNames[pos]}, getting erased`);
+        playerNames[pos] = '';
+        box.style.color = 'black';
+        box.style.border = '1px solid rgba(40, 44, 48, 0.377)';
+
+        // playerNames.splice(pos, 1);
+    } else if (bVal == playerNames[pos]) {
+        console.log(`${bVal} is already in ${bName}`);
+        //return;
+    } else {
+        if (playerNames.length > 6) {
+            alert("Already Got enough players. Click Reset to start over.");
+        } else {
+            playerNames[pos] = bVal;
+            // box.disabled = true;
+            box.style.color = 'grey';
+            box.style.border = '2px outset  rgba(0, 128, 25, 0.3)';
+            console.log('last conditon reached');
+        }
     }
-    console.log(playerNames);
+    console.log('pNames ' + playerNames);
+    console.log('pNames len' + playerNames.length);
+    console.log(`val at pos ${pos} = ${playerNames[pos]} vs ${bVal} in box`);
+
+    return playerNames;
+}
+
+
+function trimNames(names) {
+    let trimmed = names.filter(n => {
+        if (typeof n === 'string' && (n != '' && n != ' ')) {
+            console.log('passing ' + n);
+            return n;
+        }
+        console.log('filteting thisnel');
+    });
+    return trimmed;
 }
 
 function getRaceList() {
@@ -83,7 +146,7 @@ function toggleSubmit() {
     return textFields;
 }
 
-function getPlayerData(names, races) {
+function addPlayer(names, races) {
     //for each name in array, add a new player object to array
     let i = 1;
     names.forEach(name => {
@@ -91,21 +154,21 @@ function getPlayerData(names, races) {
             playername: name,
             id: i,
             gotRaces: [],
-            displayData: function () {
+            displayData: function (c) {
+                let showRaces = c === 2 ? this.gotRaces.join(', ') : this.gotRaces[0];
                 let Output = '';
                 output =
                     `<h4 class="player-header"><span class="output-id">Player ${this.id}</span></h4>
-                     <p class="player-details">${this.playername} - ${this.gotRaces[0]}</p>`;
+                     <p class="player-details">${this.playername} - ${showRaces}</p>`;
                 return output;
             },
+             //randomize/shuffle race array and select first element, then remove that from array 
             getRace: function (races) {
-                //randomize/shuffle race array and select first element, then remove that from array 
                 for (let i = races.length - 1; i > 0; i--) {
                     let j = Math.floor(Math.random() * (i + 1));
                     [races[i], races[j]] = [races[j], races[i]];
                 }
                 this.gotRaces.push(races.splice(0, 1));
-                console.log(this.gotRaces);
             }
         });
         i++;
@@ -123,67 +186,27 @@ function doleOut(races) {
     };
     console.log(races);
 }
+
 //concats the player displayData() output and pushes to DOM 
 function displays() {
     const resultList = document.querySelector('#result-list');
+    const choiceCount = document.querySelector('#choice-counter').value;
+
     let pl = players;
     let outText = [];
 
     pl.forEach(p => {
-        outText.push(p.displayData());
+        outText.push(p.displayData(choiceCount));
     });
+
     raceDisplay.innerHTML = outText.join('<br>');
     resultList.style.display = "block";
 }
 
-// eed to clear players objects, races array, clean out the result list HTML, 
+// Need to clear players objects, races array, clean out the result list HTML, 
 // Hide buttons, and clear names in textboxes.
 
 //When reset button is pressed, fire off page reload
 resetButton.addEventListener('click', () => {
     document.location.reload(true);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-// resetButton.addEventListener('click',() => {
-//     const resultList = document.querySelector('#result-list');
-
-//     players.length = 0;
-//     // raceList.length = 0;
-//     console.log(players);
-//     console.log(playerNames);
-//     console.log(selectBoxes);
-//     // playerNames.length = 0;
-//     // selectBoxes.length = 0;
-
-//     resultList.style.display = "none";
-//     raceDisplay.innerHTML = "";
-
-//     // submitButton.style.display = "none";
-
-
-
-// });
-// // function resetData() {
-    
-// // }
-*/
