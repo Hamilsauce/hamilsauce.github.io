829a9241e84c40982c1583ba20c2c9b43b9e781d
Jacob Hamilton <jake@seahag.onmicrosoft.com>
2019-11-24 22:03:40 -0600

    hf
    fgfgf

--- audio/trackListModule.js
+++ audio/trackListModule.js
@@ -1,10 +1,9 @@
 
 const ROOT_NAME = 'https://hamilsauce.github.io/';
 
- const musicFileList = [
+export const musicFileList = [
     'audio/crimsom-picture-of-a-city-09102019.mp3',
     'audio/Ink Erodes Slowly2.mp3',
-    'audio/incaSteepleDorian.mp3',
     'audio/incaSteepleDorian.mp3',
     'audio/new space reduced.mp3',
     'audio/lazystar remaster.mp3'
