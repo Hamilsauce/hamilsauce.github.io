353be6403a7cf7e74e0b3b2511dbfa4e83f50e5a
Jacob Hamilton <jake@seahag.onmicrosoft.com>
2019-11-27 03:50:59 -0600

    updated audioplayer stuf, fail

--- audio/trackListModule.js
+++ audio/trackListModule.js
@@ -1,20 +1,28 @@
-
 const ROOT_NAME = 'https://hamilsauce.github.io/';
 
 export const musicFileList = [
     'audio/crimsom-picture-of-a-city-09102019.mp3',
-    'audio/Ink Erodes Slowly2.mp3',
+    'audio/Ink%20Erodes%20Slowly2.mp3',
     'audio/incaSteepleDorian.mp3',
-    'audio/new space reduced.mp3',
-    'audio/lazystar remaster.mp3'
+    'audio/new%20space%20reduced.mp3',
+    'audio/new%20space%20guitar%20mix.mp3',
+    'audio/lazystar%20remaster.mp3'
 ];
 
-export const fullUrlList = musicFileList
-    .map(track => {
-        return `${ROOT_NAME}${track}`;
+export const songNames = musicFileList
+    .map(urlString => {
+        return urlString
+            .split('/')[1] //*  split off 'audio', then split 'mp3' off the that new array's 2nd element (everything other than 'audio')
+            .split('.')[0] //*  with the array element containings only the track name, split the text at the '%20'
+            .split('%20')
+            .join(' ');
     });
 
-console.log(fullUrlList);
+export const urlArray = musicFileList
+    .map(track => {
+        return `${ROOT_NAME}${track}`;
 
-
- //   export {fullUrlList}
+    });
+    // console.log('in trackModule, urlArray: ');
+    // console.log(urlArray);
+    // console.log(songNames);
