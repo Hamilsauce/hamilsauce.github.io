f7bcb11592a8ff74ee5105782c83d60c1f07305b
Jacob Hamilton <jake@seahag.onmicrosoft.com>
2019-12-24 14:15:24 -0600

    Add files via upload

--- ctaTrainTracker.js
+++ ctaTrainTracker.js
@@ -1,14 +1,7 @@
-import {
-    blueLineStops
-} from './ctaBlueStops.js'
-import {
-    csvToJson
-} from './csvToJson12.21.js'
-import * as fetchman from './dataFetch.js'
 
-// get stop data to populate select optuons
-// const blueStops = csvToJson(blueLineStops, 'comma');
-App.dataFetcher.fetchJson('https://hamilsauce.github.io/trainStops.json', 'allTrainStops');
+// import * as fetchman from './dataFetch.js'
+
+App.dataFetcher.fetchJson('https://hamilsauce.github.io/cta-trackers/trainStops.json', 'allTrainStops');
 
 let allStops = [];
 const timerTest = () => {
@@ -18,11 +11,9 @@ const timerTest = () => {
 
         allStops = stops.stops;
         sortAndAddStops(allStops);
-    }, 5000)
-
+    }, 4000)
 }
 timerTest();
-
 
 const sortAndAddStops = (stopList) => {
     const stopSelect = document.querySelector('.stopSelect');
@@ -35,21 +26,19 @@ const sortAndAddStops = (stopList) => {
         if (first > second) compare = 1;
         else if (first < second) compare = -1;
         return compare;
-    });
-    console.log(stopList);
+    })
+        .forEach(stop => {
+            const newOption = document.createElement('option');
 
-    stopList.forEach(stop => {
-        // let [stopName, mapId] = Object.values(stop);
-        const newOption = document.createElement('option');
-
-        newOption.classList.add('stopOption');
-        newOption.textContent = stop.STATION_DESCRIPTIVE_NAME;
-        newOption.value = stop.mapId;
-        stopSelect.appendChild(newOption);
-    });
+            newOption.classList.add('stopOption');
+            newOption.textContent = stop.STATION_DESCRIPTIVE_NAME;
+            newOption.value = stop.mapId;
+            stopSelect.appendChild(newOption);
+        });
 };
 
 /*//! object for holding/organizing random query string parts */
+// TODO Need to move this into dataFetch/dataFetcher
 let queryString = {
     trainUrl: 'http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx',
     apiKey: '3106a8d27f8b4f8fb99b7c8d895163dd',
@@ -62,12 +51,11 @@ let queryString = {
 }
 
 const getArrivalData = trainData => {
-    console.log(trainData);
-
     let stationEtas = Object.values(trainData)[0].eta
     return stationEtas;
 }
 /**!
+ // TODO Need to move this into dataFetch/dataFetcher
  * !makes the request for train data, calls above function to access data */
 const getTrainData = () => {
     let ctaData;
@@ -85,44 +73,34 @@ const getTrainData = () => {
                 let time = new Date(JSON.stringify(eta.arrT).slice(1, eta.arrT.length + 1));
                 let isApproaching = JSON.stringify(eta.isApp).slice(1, eta.isApp.length + 1) === '1' ? 'Due' : '';
                 document.querySelector('.preformat0' + i).innerHTML = JSON.stringify(eta.staNm).slice(1, eta.staNm.length + 1);
-                document.querySelector('.preformat1' + i).innerHTML = `${JSON.stringify(eta.stpDe).slice(1, eta.stpDe.length + 1)}`;
-                document.querySelector('.preformat2' + i).innerHTML = isApproaching;
-                document.querySelector('.preformat3' + i).innerHTML = `<b>ETA:</b> ${time.toLocaleTimeString()}`;
+                document.querySelector('.preformat1' + i).innerHTML = `<b>ETA:</b> ${time.toLocaleTimeString()}`;
+                document.querySelector('.preformat2' + i).innerHTML = `${JSON.stringify(eta.stpDe).slice(1, eta.stpDe.length + 1).replace('Service ', '')}`;
+                document.querySelector('.preformat3' + i).innerHTML = `<span class="approaching">${isApproaching}</span>`;
                 i++;
-
             })
             document.querySelectorAll('.eta').forEach(div => {
-
                 if (div.textContent) {
                     div.style.display = 'grid';
                 }
-
             });
         })
         .catch(err => {
             console.log(err);
         });
-
     return ctaData;
 }
 
-
 document.querySelector('.stopSelect').addEventListener('change', e => {
-    console.log(e.target.value)
-    console.log(document.querySelector('.stopSelect').value);
     queryString.mapId = e.target.value;
     let etaData = (getTrainData());
-
 })
+
 const filterByLine = (e) => {
     const boxes = document.querySelectorAll('.trainCheckbox');
     let filteredStops = [];
     boxes.forEach(box => {
-
-
         if (box.checked) {
             let lineName = box.value;
-            console.log(lineName);
 
             filteredStops = allStops
                 .filter(stop => {
