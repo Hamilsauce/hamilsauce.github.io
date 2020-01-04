cb494104e290ac56d668d5ea47631a8e89bdff00
jacobwilsonhamilton <jacobwilsonhamilton@gmail.com>
2020-01-03 18:25:38 -0600

    updated note mondo

--- notemondo/notemondo.js
+++ notemondo/notemondo.js
@@ -2,8 +2,6 @@ import { toneTable } from './noteSource.js';
 import { csvToJson } from './csvToJson.js';
 import { User } from './pword.js';
 
-
-
 const divOut = document.querySelector('.note-table');
 const noteTable = document.querySelector('.noteTable');
 const inputVal = document.querySelector('.noteInput');
@@ -15,69 +13,47 @@ const editButton = document.querySelector('.editButton');
 const modalContainer = document.querySelector('.modal-container');
 const dimmer = document.querySelector('.dimmer');
 const loginModal = document.querySelector('.loginModal');
-
+const cancelButton = document.querySelector('.pwCancel');
 
 const jake = new User('farts');
 console.log(jake.password)
 
-//**csv conversion**
-const mapTheData = (n) => {
-  let mappedData = [];
-  n.forEach(curr => {
-    // console.log(curr);
+//log: dataset/table construction functions
 
-    let entries = Object.entries(curr); // <-- replace n[3] witth query (n[?])
-    let props = entries
-      .map(e => {
-        let [k, v] = e;
-        let key = k.trim();
-        let val = v.trim();
-        return `<li class="note-prop">${key}: ${val}</li>`;
-      });
-    mappedData.push(props);
-  });
-  return mappedData;
-}
-
-const mapTheData2 = (n) => {
+//! map note data values/prop vals/fields to html
+const mapTheData = (arrOfNoteObjects) => {  //* takes value returned from CsvToJson
   let mappedData = [];
-  n.forEach(curr => {
+  arrOfNoteObjects.forEach(curr => {
     let i = 0;
-    let entries = Object.values(curr); // <-- replace n[3] witth query (n[?])
+    let entries = Object.values(curr); //* for Each note obj in array, get prop values
     let props = entries
-      .map(e => {
+      .map(e => {                      //* for each prop value, embed in td markup string
         let val = e.trim();
         return `<td class="note-prop row${++i}">${val}</td>`;
       });
     i = 0;
-    mappedData.push(props);
+    mappedData.push(props); //* store array of tds (3, 1 for each note obj prop) in array
   });
   return mappedData
-  console.log(mappedData);
-
 }
 
-let reducer = (arr) => {
-  let tableArray = [];
-
-  arr.forEach(row => {
-    let tRow = document.createElement('tr');
-    tRow.innerHTML = row.join('');
-
-    document.querySelector('.noteTable').appendChild(tRow);
+//! take array of td arrays > reduce td to rows > rows to data body
+let reduceAndAppendRows = (arr) => {
+  arr.forEach(row => {                          //*for each array of tds in array
+    let tRow = document.createElement('tr'); //*create a row
+    tRow.innerHTML = row.join('');            //* set row's markup as td's reduced to single string
+    document.querySelector('.noteTable').appendChild(tRow); //* slap that bad boy on the end of the table
   })
 };
+//Old row reducer
+// let reducer2 = (arr) => {
+//   let joined = arr.reduce((item, acc) => {
+//     return acc += item;
+//   }, '');
+//   return joined;
+// }
 
-let reducer2 = (arr) => {
-  //reduce array of html elems to single string
-  let joined = arr.reduce((item, acc) => {
-    return acc += item;
-  }, '');
-  return joined;
-}
-
-//query function
-const filterData = (data, query) => {
+const filterData = (data, query) => { //! makes new array from CsvToJson return value of notes matching search
   let filterResults = data
     .filter(noteObj => {
       return noteObj.Note.toUpperCase()
@@ -85,18 +61,17 @@ const filterData = (data, query) => {
     });
   return filterResults;
 }
-
+//log: end dataset/table construction fns
 
 (() => {
-  //build initial table at page load, then set event handler for queries
+  //! build initial table at page load...
   let noteData = csvToJson(toneTable, 'tab');
   let mapped = mapTheData2(noteData);
   let reduced = reducer(mapped);
 
-  userform.addEventListener('submit', e => {
+  //!  then set event handler for queries
+  userform.addEventListener('submit', e => {  //* replace existing table/markup/data w/ headers only (clear out old data)
     e.preventDefault();
-    //clear existing data from table so that only filtered rows will display, rather than
-    //not clearing and old data exisitng  with filtered data
     document.querySelector('.noteTable').innerHTML = /*html*/ `
       <tr class="colNames">
         <th>Note</th>
@@ -104,20 +79,17 @@ const filterData = (data, query) => {
         <th>Wavelength</th>
       </tr>
     `;
-
-    let filterNote = filterData(noteData, inputVal.value)
-    let mapped = mapTheData2(filterNote)
-    let reduced = reducer(mapped);
-
-    // noteTable.innerHTML = reduced;
+    //! execute!
+    let filterNote = filterData(noteData, inputVal.value)  //* 1) filter note data according to user input,
+    let mapped = mapTheData(filterNote)                   //*  2) map note obj props to sets of td elements,
+    reduceAndAppendRows(mapped);                          //* 3) reduce sets of tds to rows, append to table
   })
-
 })();
-const msgModal = document.querySelector('.msgModal');
 
+//! UI/Dom/Modal/Event handlingB
+const msgModal = document.querySelector('.msgModal');
 const displayMsg = authMsg => {
   const msgText = document.querySelector('.authText');
-
   msgText.textContent = authMsg;
   msgModal.style.display = 'flex';
 
@@ -126,37 +98,34 @@ const displayMsg = authMsg => {
     modalContainer.style.display = 'none';
   }, 3000);
 }
+
 msgModal.addEventListener('click', e => {
   modalContainer.style.display = 'none';
 });
 
-
+//!password submit button
 pwSubmit.addEventListener('click', e => {
   e.preventDefault();
-  let authMsg = jake.checkLogin(pwField.value)
+  let authMsg = jake.checkLogin(pwField.value)  //* tests input against stored pword
   let authStatus = jake.authorized;
 
   if (authStatus) {
     document.querySelectorAll('.note-prop')
-    .forEach(td => {
-      td.contentEditable = true;
-      td.classList.add('editable');
-    });
+      .forEach(td => {
+        td.contentEditable = true;
+        td.classList.add('editable');
+      });
   }
+
   setTimeout(() => {
     loginModal.style.display = 'none';
     displayMsg(authMsg);
-
   }, 500);
 
   pwField.value = '';
-  console.log(authStatus)
-
-  // jake.password.validateInput();
+  //TODO jake.password.validateInput();
 });
 
-
-const cancelButton = document.querySelector('.pwCancel');
 cancelButton.addEventListener('click', e => {
   modalContainer.style.display = 'none';
   dimmer.style.display = 'flex';
