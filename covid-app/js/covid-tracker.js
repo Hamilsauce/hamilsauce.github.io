5b3828157b9c28864b353504399153d2bd9523b7
jacobwilsonhamilton <jacobwilsonhamilton@gmail.com>
2020-04-02 17:00:10 -0500

    upates

--- covid-app/js/covid-tracker.js
+++ covid-app/js/covid-tracker.js
@@ -4,8 +4,6 @@
 
 const covidFetch = () => {
     const outDiv = document.querySelector('.dataDisplay');
-    console.log('it happened');
-
     var raw = "";
     var requestOptions = {
         method: 'GET',
@@ -13,25 +11,56 @@ const covidFetch = () => {
         redirect: 'follow'
     };
 
-    fetch("https://covidapi.info/api/v1/country/USA/2020-03-15")
+    fetch("https://covidapi.info/api/v1/country/USA")
         .then(res => res.json())
         .then(data => {
-            console.log(data);
+            let [date, latestData] = Object.entries(data.result).pop();
+            let latestDate = new Date(date).toLocaleDateString();
 
-            let covidData = JSON.stringify(data,  undefined, '\t')
-            outDiv.textContent = covidData;
+            // let covidData = JSON.stringify(latestData,  undefined, '\t')
+            let covidData = formatDateData(latestData);
+
+            document.querySelector('.inner.cover').classList.add('collapsed');
+            setTimeout(() => {
+                outDiv.innerHTML =
+                `
+                <h4>United States as of ${latestDate}</h4>
+                ${covidData}
+            `;
+            document.querySelector('.dataDisplay').classList.add('show')
+            }, 750);
+
+
+            // document.querySelector('.inner.cover').style.height = '0px';
+
         })
         .catch(err => {
             console.log(err);
         });
 }
 
+const formatDateData = dateData => {
+    const details = Object.entries(dateData)
+        .map(([key, val]) => {
+            let detail = `${key.charAt(0).toUpperCase() + key.slice(1)}: ${Number(val).toLocaleString()} `
 
+            return `<li>${detail}</li>`;
+        }).reduce((acc, curr) => {
+            let list = acc += curr;
+            return list;
+        });
+    return `
+        <ul class="detail-list">
+            ${details}
+        </ul>
+    `;
+}
 
 document.querySelector('#submitButton')
     .addEventListener('click', e => {
         // convertData()
-        covidFetch()
+        covidFetch();
+
     })
 
 
