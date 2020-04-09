c1c175a7e8d8c778041968c241a4e2e1fba52629
jacobwilsonhamilton <jacobwilsonhamilton@gmail.com>
2020-04-09 07:46:17 -0500

    cool2

--- vue-datatable/app.js
+++ vue-datatable/app.js
@@ -2,111 +2,118 @@
 
 // register the grid component
 Vue.component('demo-grid', {
- template: '#grid-template',
- props: {
-  heroes: Array,
-  columns: Array,
-  filterKey: String
- },
- data: function() {
-  let sortOrders = {}
-  this.columns.forEach(function(key) {
-   sortOrders[key] = 1;
-  })
-  return {
-   sortKey: '',
-   sortOrders: sortOrders,
-   searchQuery: ''
-  }
- },
- computed: {
-  filteredHeroes: function() {
-   let sortKey = this.sortKey
-   let filterKey = this.filterKey && this.filterKey.toLowerCase()
-   let order = this.sortOrders[sortKey] || 1
-   let heroes = this.heroes
-   if (filterKey) {
-    heroes = heroes.filter(function(row) {
-     return Object.keys(row).some(function(key) {
-      return String(row[key]).toLowerCase().indexOf(filterKey) > -1
-     })
+  template: '#grid-template',
+  props: {
+    heroes: Array,
+    columns: Array,
+    filterKey: String
+  },
+  data: function () {
+    let sortOrders = {}
+    this.columns.forEach(function (key) {
+      sortOrders[key] = 1;
     })
-   }
-   if (sortKey) {
-    heroes = heroes.slice().sort(function(a, b) {
-     a = a[sortKey]
-     b = b[sortKey]
-     return (a === b ? 0 : a > b ? 1 : -1) * order
-    })
-   }
-   return heroes
+    return {
+      sortKey: '',
+      sortOrders: sortOrders,
+      searchQuery: ''
+    }
+  },
+  computed: {
+    filteredHeroes: function () {
+      let sortKey = this.sortKey
+      let filterKey = this.filterKey && this.filterKey.toLowerCase()
+      let order = this.sortOrders[sortKey] || 1
+      let heroes = this.heroes
+      if (filterKey) {
+        heroes = heroes.filter(function (row) {
+          return Object.keys(row).some(function (key) {
+            return String(row[key]).toLowerCase().indexOf(filterKey) > -1
+          })
+        })
+      }
+      if (sortKey) {
+        heroes = heroes.slice().sort(function (a, b) {
+          a = a[sortKey]
+          b = b[sortKey]
+          return (a === b ? 0 : a > b ? 1 : -1) * order
+        })
+      }
+      return heroes
+    }
+  },
+  filters: {
+    capitalize: function (str) {
+      return str.charAt(0).toUpperCase() + str.slice(1)
+    }
+  },
+  methods: {
+    sortBy: function (key) {
+      this.sortKey = key
+      this.sortOrders[key] = this.sortOrders[key] * -1
+    }
   }
- },
- filters: {
-  capitalize: function(str) {
-   return str.charAt(0).toUpperCase() + str.slice(1)
-  }
- },
- methods: {
-  sortBy: function(key) {
-   this.sortKey = key
-   this.sortOrders[key] = this.sortOrders[key] * -1
-  }
- }
 });
 
 const demo = new Vue({
-   el: "#vue-app",
-   data: function() {
+  el: "#vue-app",
+  data: function () {
     return {
-     heading: 'Dynamic sortable table',
-     gridColumns: ['markerName','markerSlug'],
-     
-     gridData: koroks,
-     
-     //  { name: 'Chuck Norris', power: Infinity },
-     //  { name: 'Bruce Lee', power: 9000 },
-     //  { name: 'Jackie Chan', power: 7000 },
-     //  { name: 'Jet Li', power: 8000 }
-     //  ],
-     searchQuery: ''
+      heading: 'Botw Koroks',
+      gridColumns: ['markerName', 'lat', 'lng'],
+      gridData: '',
+      searchQuery: ''
     }
-   },
-   computed: {
-    korokCount() {
-     return koroks.length;
+  },
+  methods: {
+    getSeeds() {
+      fetch('https://hamilsauce.github.io/koroks2.json')
+      .then(res => res.json())
+        .then(data => {
+        this.gridData = this.slimTheSeeds(data);
+      })
     },
-    slimTheSeeds() {
-     const keepList = [
-       'lat',
-       'id',
-       'lng',
-       'markerName',
-       'markerSlug',
-       'wikiPag'
-     ];
-     const slimSeeds = koroks
-      .map(korok => {
-       let id = koroks.indexOf(korok);
-       korok.id = id;
+    slimTheSeeds(data) {
+      const keepList = [
+        'lat',
+        'id',
+        'lng',
+        'markerName',
+        'markerSlug',
+        'wikiPag'
+      ];
+      let koroks = data['koroks'];
 
-       let slimSeed = Object.entries(korok)
-        .filter(([prop, val]) => {
-         if (keepList.includes(prop) === true) {
-      console.log([prop, val]);
-          return [prop, val];
-         }
-        }).reduce((obj, item) => {
-         obj[item[0]] = item[1];
-         return obj;
-        }, {});
-      console.log(slimSeed);
-       return slimSeed;
-      });
-      
-      console.log('slimSeeds');
-      console.log(slimSeeds);
-     return slimSeeds;
+      const slimSeeds = data['koroks'].map(korok => {
+          let id = koroks.indexOf(korok);
+          korok.id = id;
+
+          let slimSeed = Object.entries(korok)
+            .filter(([prop, val]) => {
+              if (keepList.includes(prop) === true) {
+                // console.log([prop, val]);
+                return [prop, val];
+              }
+            }).reduce((obj, item) => {
+              obj[item[0]] = item[1];
+              return obj;
+            }, {});
+          // console.log(slimSeed);
+          return slimSeed;
+        });
+
+      return slimSeeds;
     }
-   }
+  },
+  watch: {
+
+  },
+  created() {
+    this.getSeeds();
+  },
+  computed: {
+    korokCount() {
+      return this.gridData.length;
+    }
+  }
 });
