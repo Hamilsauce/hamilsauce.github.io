f954b6f3c2c43001152a9dfc8dde05f4c75ba310
jacobwilsonhamilton <jacobwilsonhamilton@gmail.com>
2020-05-28 15:59:20 -0500

    gloomy

--- gloom-form/gloomguy.js
+++ gloom-form/gloomguy.js
@@ -1,3 +1,12 @@
+const countDisplay = document.querySelector('.counter-display');
+const countAdd = document.querySelector('.add');
+const countMinus = document.querySelector('.minus');
+const cards = document.querySelectorAll('.card');
+const minusLevel = document.querySelector('.minusLevel');
+const addLevel = document.querySelector('.addLevel');
+const lvlCard = document.querySelector('.level-card');
+const lvlButton = document.querySelectorAll('.lvlButton');
+
 class Character {
   constructor(name, charClass, level) {
     this.name = name,
@@ -17,26 +26,19 @@ class Character {
   }
   buyItem() {} //will need to subtract gold + add card}
   addCard() {}
-
 }
 
 const factory = (name, charClass, level) => {
-
+  const newChar = new Character(name, charClass, level);
+  return newChar
 }
+
+const char1 = factory('frodo', 'hobbit', 2);
+
 const ui = {
-  activeCell: null,
+  activecard: null,
   activeClickCount: 0
 }
-const countDisplay = document.querySelector('.counter-display');
-const countAdd = document.querySelector('.add');
-const countMinus = document.querySelector('.minus');
-const cells = document.querySelectorAll('.cell');
-const minusLevel = document.querySelector('.minusLevel');
-const addLevel = document.querySelector('.addLevel');
-const lvlCell = document.querySelector('.level-cell');
-const lvlButton = document.querySelectorAll('.lvlButton');
-
-const char1 = new Character('frodo', 'hobbit', 2);
 
 
 const getSaved = () => {
@@ -46,62 +48,86 @@ const saveChar = () => {
   let saveChar = localStorage.getItem('')
 }
 
-lvlCell.addEventListener('click', e => {
+lvlCard.addEventListener('click', e => {
   lvlButton.forEach(btn => {
     btn.classList.remove('buttonhide')
     btn.classList.add('buttonShow')
   })
 })
-lvlCell.addEventListener('onblur', e => {
+
+lvlCard.addEventListener('onblur', e => {
   // lvlButton.forEach(btn => {
   // btn.classList.add('buttonhide')
   //btn.classList.remove('buttonShow')
   //  })
 })
-countAdd.addEventListener('click', () => {
+
+countAdd.addEventListener('click', () => { //!incrementer code
   char1.addGold(1);
   countDisplay.textContent = char1.gold;
 });
-countMinus.addEventListener('click', () => {
+countMinus.addEventListener('click', () => { //!incrementer code
   char1.removeGold(1);
   countDisplay.textContent = char1.gold;
 });
 
-const focuser = () => {
-  const cells = document.querySelectorAll('.cell');
- 
-  cells.forEach(c => {
-    let input = c.children[1];
-    let focused = false
+const cardFocusHandler = () => { //! handles card clicks
+  const cards = document.querySelectorAll('.card');
+  cards.forEach(card => {
+    let input = card.querySelector('.textIn');
+    handleTextInClick(input)
 
-    c.addEventListener('click', e => {
+    card.addEventListener('click', e => {
       e.stopPropagation();
-   
-      let lastTarget = ui.activeCell || c;
-      ui.activeCell = c;
-  
-      if (ui.activeCell == lastTarget) {
-        if (ui.activeClickCount == 1) {
-          input.focus();
-          focused = true;
-        }
-   
-        if (focused === true) {
-          ui.activeCell.classList.remove('cell')
-          ui.activeCell.classList.add('activeCell')
-        }
-        ui.activeClickCount += 1;
-      }
-
-      if (ui.activeCell != lastTarget) { //If current target is new cell, reset clicks and change uistste
-        ui.activeCell = c;
-        focused = false
-
-        ui.activeClickCount = 1
-        lastTarget.classList.remove('activeCell')
-        lastTarget.classList.add('cell')
-      }
+      handleCardState(card)
     })
   })
 }
-focuser()
+
+const handleCardState = card => { //! handles card behavior
+  //* tracks user card selection as well as click counts, adjusts behavior accordingly
+  let lastTarget = ui.activeCard || card;
+  let input = card.querySelector('.textIn');
+  let focused = false
+  ui.activeCard = card;
+
+  if (ui.activeCard == lastTarget) {
+    if (ui.activeClickCount == 1) {
+      console.log('focus input');
+      input.focus();
+      focused = true;
+    }
+    if (focused === true) {
+      console.log('focus true, adding activeCard, remove card');
+      ui.activeCard.classList.add('activeCard')
+      ui.activeCard.querySelector('.textIn').style.pointerEvents = 'all';
+    }
+    if (focused === true && ui.activeClickCount >= 2) {
+      console.log('focus true was true and clicks > 2, adding activeCard, remove card');
+      ui.activeCard.classList.remove('activeCard')
+      ui.activeCard.classList.add('card')
+      ui.activeCard.querySelector('.textIn').style.pointerEvents = 'none';
+      ui.activeClickCount = 0;
+      focus = false;
+    }
+    ui.activeClickCount += 1;
+  }
+  if (ui.activeCard != lastTarget) { //If current target is new card, reset clicks and change uistste
+    console.log('focus false, removing activeCard class');
+    ui.activeCard = card;
+    focused = false
+    ui.activeClickCount = 1
+    lastTarget.classList.remove('activeCard')
+    lastTarget.classList.add('card')
+    ui.activeCard.querySelector('.textIn').style.pointerEvents = 'none';
+  }
+}
+
+const handleTextInClick = textIn => {
+  textIn.addEventListener('click', e => {
+    e.stopPropagation()
+    e.preventDefault();
+  })
+}
+
+cardFocusHandler()
