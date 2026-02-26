c7cf147b881676c37193d12befbd7c79dd1eb61a
Jacob Hamilton <jake@seahag.onmicrosoft.com>
2026-02-26 17:04:04 -0600

    Update fetchRepoData to fetch all repos with pagination
    Refactor fetchRepoData to handle pagination and fetch all repositories.

--- hamhome3/js/repo-data.js
+++ hamhome3/js/repo-data.js
@@ -1,5 +1,19 @@
-const apiUrl = 'https://api.github.com/users/hamilsauce/repos?per_page=100&type=owner';
+const apiUrl = 'https://api.github.com/users/hamilsauce/repos';
 
 export const fetchRepoData = async (url = apiUrl) => {
-  return await (await fetch(url)).json();
+  let page = 1
+  let done = false
+  let repos = []
+  
+  while (!done) {
+    const apiUrlQuery = `https://api.github.com/users/hamilsauce/repos?per_page=100&page=${page}`;
+    
+    const res = await (await fetch(apiUrlQuery)).json();
+    
+    repos = repos.concat(res);
+    done = res.length < 100;
+    page++;
+  }
+  
+  return repos
 };
